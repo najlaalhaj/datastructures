e088b32155d1b51c616548732313f962ba576d78
John-John Tedro <udoprog@spotify.com>
2018-06-20 12:52:42 +0200

    move to Maven

---
+++ src/test/java/com/datastructure/LinkedListTest.java
@@ -0,0 +1,12 @@
+package com.datastructure;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+
+  @Test
+  public void testSomething() {
+    final LinkedList list = new LinkedList();
+    /* test something */
+  }
+}
